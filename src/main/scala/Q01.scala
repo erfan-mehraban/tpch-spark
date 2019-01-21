5940a4f14acee1e1d70f5f00f7636a9489b97403
erfan.mehraban <erfan.mehraban@gmail.com>
2019-01-21 17:55:14 +0330

    format query01

--- src/main/scala/Q01.scala
+++ src/main/scala/Q01.scala
@@ -20,12 +20,18 @@ class Q01 extends TpchQuery {
     val decrease = udf { (x: Double, y: Double) => x * (1 - y) }
     val increase = udf { (x: Double, y: Double) => x * (1 + y) }
 
-    schemaProvider.lineitem.filter($"l_shipdate" <= "1998-09-01")
+    schemaProvider.lineitem
+      .filter($"l_shipdate" <= "1998-09-01")
       .groupBy($"l_returnflag", $"l_linestatus")
-      .agg(sum($"l_quantity"), sum($"l_extendedprice"),
+      .agg(
+        sum($"l_quantity"),
+        sum($"l_extendedprice"),
         sum(decrease($"l_extendedprice", $"l_discount")),
         sum(increase(decrease($"l_extendedprice", $"l_discount"), $"l_tax")),
-        avg($"l_quantity"), avg($"l_extendedprice"), avg($"l_discount"), count($"l_quantity"))
+        avg($"l_quantity"),
+        avg($"l_extendedprice"),
+        avg($"l_discount"),
+        count($"l_quantity"))
       .sort($"l_returnflag", $"l_linestatus")
   }
 }
